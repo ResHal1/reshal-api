80a613d75f13e64c4e564577a6383c1eb02e8261
bartosz121 <bmagiera121@gmail.com>
2023-04-25 22:46:59 +0200

    RES-14 Use lifespan/app config

--- reshal_api/main.py
+++ reshal_api/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from reshal_api.config import CORSSettings, UvicornSettings, get_config
+from reshal_api.lifespan import lifespan
+
+config = get_config()
+
+app = FastAPI(
+    lifespan=lifespan,
+    title=config.TITLE,
+    version=config.VERSION,
+    root_path=config.ROOT_PATH,
+    debug=config.ENVIRONMENT.is_local,
+    docs_url=None if config.ENVIRONMENT.is_production else "/docs",
+    redocs_url=None if config.ENVIRONMENT.is_production else "/docs",
+    openapi_url=None if config.ENVIRONMENT.is_production else "/openapi.json",
+)
+
+app.add_middleware(CORSMiddleware, **CORSSettings().dict())
 
 
 @app.get("/")
@@ -11,7 +28,7 @@ async def home():
 def run():
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("reshal_api.main:app", **UvicornSettings().dict())
 
 
 if __name__ == "__main__":
