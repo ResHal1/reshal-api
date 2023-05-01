2d52c15a4738d529439021d86fb1715d9ba30469
bartosz121 <bmagiera121@gmail.com>
2023-05-01 22:40:52 +0200

    RES-21 Use reservations, payments, timeframes routers

--- reshal_api/main.py
+++ reshal_api/main.py
@@ -6,6 +6,9 @@ from reshal_api.auth.router import router as auth_router
 from reshal_api.config import CORSSettings, UvicornSettings, get_config
 from reshal_api.facility.router import router as facility_router
 from reshal_api.lifespan import lifespan
+from reshal_api.reservation.router import router as reservation_router
+from reshal_api.timeframe.router import router as timeframe_router
+from reshal_api.payment.router import router as payment_router
 
 config = get_config()
 
@@ -24,6 +27,9 @@ app.add_middleware(CORSMiddleware, **CORSSettings().dict())
 app.mount("/static", StaticFiles(directory=config.STATIC_DIR), name="static")
 app.include_router(auth_router, prefix="/auth")
 app.include_router(facility_router, prefix="/facilities")
+app.include_router(reservation_router, prefix="/reservations")
+app.include_router(timeframe_router, prefix="/timeframes")
+app.include_router(payment_router, prefix="/payments")
 
 
 @app.get("/")
