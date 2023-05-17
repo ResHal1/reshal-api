1e4688d053709024a734a5aba6bdd84c992bf2b7
bartosz121 <bmagiera121@gmail.com>
2023-05-17 22:14:57 +0200

    Add `EndpointFilter` class, filter out `/metrics` logs

--- reshal_api/lifespan.py
+++ reshal_api/lifespan.py
@@ -1,11 +1,23 @@
+import logging
 from contextlib import asynccontextmanager
+from logging import LogRecord
 
 from fastapi import FastAPI
 
 from .database import async_engine
 
 
+class EndpointFilter(logging.Filter):
+    def __init__(self, *args, path: str, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._path = path
+
+    def filter(self, record: LogRecord) -> bool:
+        return record.getMessage().find(self._path) == -1
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logging.getLogger("uvicorn.access").addFilter(EndpointFilter(path="/metrics"))
     yield
     await async_engine.dispose()
